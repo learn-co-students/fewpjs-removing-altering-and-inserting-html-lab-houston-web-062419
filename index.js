cf85ce73d4bf12482f6a3f3585eb71530aef7e40
Aaron Briscoe <abriscoe2008@gmail.com>
2019-11-21 02:53:03 -0600

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 let main = document.querySelector('#main')
 main.remove()
 
-// let newHeader = document.createElement('h1')
-// newHeader.id = 'victory'
-// newHeader.innerText = 'Aaron is the champion'
-
-const newHeader = document.createElement("h1");
+let newHeader = document.createElement("h1");
 newHeader.id = "victory"
-newHeader.innerHTML = "Flatiron School is the champion!";
-newHeader.className = "victory";
+newHeader.innerHTML = "Aaron School is the champion!";
+
