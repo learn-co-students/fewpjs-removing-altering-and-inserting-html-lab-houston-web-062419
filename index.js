b189c3a7e4168647429eec0c9f424a67e42e02f3
Aaron Briscoe <abriscoe2008@gmail.com>
2019-11-21 02:49:10 -0600

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let main = document.querySelector('#main')
 main.remove()
 
-let newHeader = document.createElement('h1')
-newHeader.setAttribute("id", "victory")
+let newHeader = document.createElement('h1').id = 'victory'
+
 newHeader.innerText= 'is the champion'
